ab58149eafcf9d078b1ca6c49f71e64b4bc80f31
Jesús Aguirre <66501518+aguirre-jes@users.noreply.github.com>
2024-07-12 03:27:13 +0000

    chore: Update sonar-project.properties version to 1.12.0 and exclude __init__.py from SonarQube analysis

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,25 @@ from main import app
 
 client = TestClient(app)
 
+def test_days_between_dates():
+    data = {
+        "date1": "2023-01-01",
+        "date2": "2023-01-31"
+    }
+    response = client.post("/days_between_dates", json=data)
+    assert response.status_code == 200
+    assert response.json()["days_between"] == 30
+
+def test_string_to_datetime():
+    data = {
+        "date_string": "2023-01-01",
+        "date_format": "%Y-%m-%d"
+    }
+    response = client.post("/string_to_datetime", json=data)
+    assert response.status_code == 200
+    # Asegúrate de que la fecha resultante sea correcta según la lógica de tu aplicación
+    assert response.json()["result_date"] == "2023-01-01T00:00:00"
+
 def test_generate_current_datetime():
     response = client.get("/generate_current_datetime")
     assert response.status_code == 200
