a9b0f8802be06c9b56805bd79694fbd4e557467f
aguirre-jes <66501518+aguirre-jes@users.noreply.github.com>
2024-08-06 20:12:43 -0500

    chore: update Apache 2.0 license URL in main.py

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
     },
     license_info={
         "name": "Apache 2.0",
-        "url": "http://www.apache.org/licenses/LICENSE-2.0.html",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
 )
 
